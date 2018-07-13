20c8a05563f76f8a2cf7577f0989b303aa582c80
Piotr Górski <piotrek.gorski90@gmail.com>
2018-07-13 11:43:30 +0200

    Add basic files for unity snake game

---
+++ krakoska-Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour {
+
+    public GameObject snakePrefab;
+    public Snake Head;
+    public Snake Tail;
+    public int NESW;  //directions
+    public Vector2 nextPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
